61956d26dc1712483280f88529315512980db1e3
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-06-26 13:44:05 +0700

    tạo login route handler

---
+++ client/src/app/api/auth/login/route.ts
@@ -0,0 +1,47 @@
+import AuthApiRequest from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from 'jsonwebtoken'
+import { HttpError } from "@/lib/http";
+
+export async function POST(request: Request) {
+    const body = (await request.json()) as LoginBodyType;
+    // Sửa lỗi: Bỏ `await` vì cookies() là hàm đồng bộ
+    const cookieStore = await cookies()
+    try {
+        const { payload } = await AuthApiRequest.sLogin(body);
+        const { accessToken, refreshToken } = payload.data
+
+        const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+        const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+
+        cookieStore.set('accessToken', accessToken, {
+            httpOnly: true,
+            path: '/',
+            expires: new Date(decodedAccessToken.exp * 1000), // Dùng new Date() để rõ ràng hơn
+            secure: true,
+            sameSite: 'lax'
+        })
+        cookieStore.set('refreshToken', refreshToken, {
+            httpOnly: true,
+            path: '/',
+            expires: new Date(decodedRefreshToken.exp * 1000),
+            secure: true,
+            sameSite: 'lax'
+        })
+
+        return Response.json(payload)
+    } catch (error) {
+        if (error instanceof HttpError) {
+            return Response.json(error.payload, {
+                status: error.status
+            })
+        } else {
+            return Response.json({
+                message: 'Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau.',
+            }, {
+                status: 500
+            })
+        }
+    }
+}
