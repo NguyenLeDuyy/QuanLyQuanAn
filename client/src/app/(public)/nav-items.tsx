6b4c49095a296a583091b4afa4e54d88e4221ba3
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-06-27 00:05:41 +0700

    dùng middleware để điều hướng request người dùng

--- client/src/app/(public)/nav-items.tsx
+++ client/src/app/(public)/nav-items.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { getAccessTokenFromLocalStorage } from '@/lib/utils'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const menuItems = [
   {
     title: 'Món ăn',
-    href: '/menu'
+    href: '/menu' // Chưa đăng nhập thì vẫn có thể xem menu
   },
   {
     title: 'Đơn hàng',
@@ -14,17 +16,32 @@ const menuItems = [
   {
     title: 'Đăng nhập',
     href: '/login',
-    authRequired: false
+    authRequired: false // Chưa đăng nhập thì mới hiển thị
   },
   {
     title: 'Quản lý',
     href: '/manage/dashboard',
-    authRequired: true
+    authRequired: true // Chỉ hiển thị khi đã đăng nhập
   }
 ]
 
+// Server: Món ăn, Đơn hàng, Đăng nhập. Do sever không biết người dùng đã đăng nhập hay chưa
+// Client: Đầu tiên sẽ hiển thị Món ăn, Đơn hàng, Đăng nhập. 
+// Sau đó Món ăn, Đơn hàng, Đăng nhập, Quản lý sẽ được hiển thị 
+// Do client biết người dùng đã đăng nhập hay chưa thông qua access token trong localStorage
+
 export default function NavItems({ className }: { className?: string }) {
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect(() => {
+    setIsAuth(Boolean(getAccessTokenFromLocalStorage()))
+  }, [])
+
   return menuItems.map((item) => {
+    if (item.authRequired === false && isAuth || item.authRequired === true && !isAuth) {
+      return null
+    }
+
     return (
       <Link href={item.href} key={item.href} className={className}>
         {item.title}
