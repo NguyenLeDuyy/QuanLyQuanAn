9c280df2601e82c9198b1ca7daaa779dd4b89279
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-07-06 23:33:02 +0700

    get me profile

--- client/src/app/manage/dropdown-avatar.tsx
+++ client/src/app/manage/dropdown-avatar.tsx
@@ -13,7 +13,9 @@ import Link from 'next/link'
 import { useLogoutMutation } from '@/queries/useAuth'
 import { handleErrorApi } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
+import { useAccountProfile } from '@/queries/useAccount'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const account = {
   name: 'Nguyễn Văn A',
   avatar: 'https://i.pravatar.cc/150'
@@ -22,6 +24,8 @@ const account = {
 export default function DropdownAvatar() {
   const logoutMutation = useLogoutMutation()
   const router = useRouter()
+  const { data } = useAccountProfile()
+  const account = data?.payload.data
   const logout = async () => {
     if (logoutMutation.isPending) return
     try {
@@ -39,13 +43,13 @@ export default function DropdownAvatar() {
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon' className='overflow-hidden rounded-full'>
           <Avatar>
-            <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
-            <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+            <AvatarImage src={account?.avatar ?? undefined} alt={account?.name} />
+            <AvatarFallback>{account?.name.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
+        <DropdownMenuLabel>{account?.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <Link href={'/manage/setting'} className='cursor-pointer'>
