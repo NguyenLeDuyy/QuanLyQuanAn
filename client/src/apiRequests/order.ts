490f888170da649399f7e14be3c5f8e0d0ada0d9
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-07-21 16:05:18 +0700

    Khai báo các API order và hiển thị data lên UI

--- client/src/apiRequests/order.ts
+++ client/src/apiRequests/order.ts
@@ -1,9 +1,14 @@
 import http from "@/lib/http";
-import { CreateOrdersResType, UpdateOrderBodyType, UpdateOrderResType } from "@/schemaValidations/order.schema";
+import queryString from 'query-string';
+import { GetOrderDetailResType, GetOrdersQueryParamsType, GetOrdersResType, UpdateOrderBodyType, UpdateOrderResType } from "@/schemaValidations/order.schema";
 
 const orderApiRequest = {
-    getOrderList: () => http.get<CreateOrdersResType>('/orders'),
+    getOrderList: (queryParams: GetOrdersQueryParamsType) => http.get<GetOrdersResType>('/orders?' + queryString.stringify({
+        fromDate: queryParams?.fromDate?.toISOString(),
+        toDate: queryParams?.toDate?.toISOString(),
+    })),
     updateOrder: (orderId: number, body: UpdateOrderBodyType) => http.put<UpdateOrderResType>(`/orders/${orderId}`, body),
+    getOrderDetail: (orderId: number) => http.get<GetOrderDetailResType>(`/orders/${orderId}`)
 
 }
 
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -11,6 +11,8 @@ import { twMerge } from "tailwind-merge"
 import jwt from "jsonwebtoken";
 import { TokenPayload } from "@/types/jwt.types"
 import guestApiRequest from "@/apiRequests/guest"
+import { BookX, CookingPot, HandCoins, Loader, Truck } from "lucide-react"
+import { format } from "date-fns"
 
 
 export function cn(...inputs: ClassValue[]) {
@@ -168,3 +170,32 @@ export const checkAndRefreshToken = async (
     }
   }
 };
+
+
+export function removeAccents(str: string) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+}
+
+export const simpleMatchText = (fullText: string, matchText: string) => {
+  return removeAccents(fullText.toLowerCase()).includes(removeAccents(matchText.trim().toLowerCase()))
+}
+
+export const formatDateTimeToLocaleString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss dd/MM/yyyy')
+}
+
+export const formatDateTimeToTimeString = (date: string | Date) => {
+  return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss')
+}
+
+export const OrderStatusIcon = {
+  [OrderStatus.Pending]: Loader,
+  [OrderStatus.Processing]: CookingPot,
+  [OrderStatus.Rejected]: BookX,
+  [OrderStatus.Delivered]: Truck,
+  [OrderStatus.Paid]: HandCoins
+}
